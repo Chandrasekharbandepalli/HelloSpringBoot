17558cf2a17d3c0ccc5cdb0504c994c3c961d920
Chandra Sekhar Bandepalli <chandrasekharbandepalli@gmail.com>
2022-08-09 17:00:30 +0530

    First Commit

---
+++ src/main/java/com/spring/hellospring/HelloSpring/Controller/Controller.java
@@ -0,0 +1,14 @@
+package com.spring.hellospring.HelloSpring.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+
+}
